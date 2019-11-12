fc9fbfec26adf2d717e837cef6debd25144e447d
Alumno <Alumno@PC35>
2019-11-12 10:23:32 -0600

    Cambias la e de Lenguague

--- More/More/MainWindow.xaml.cs
+++ More/More/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Resources; //Agregar libreria para poder trabajar con los recursos
 
 namespace More
 {
@@ -20,9 +21,27 @@ namespace More
     /// </summary>
     public partial class MainWindow : Window
     {
+        string strLenguague = "";
+        Boolean boolInt = true;
         public MainWindow()
         {
             InitializeComponent();
+            boolInt = false;
+        }
+        private void Window_Loaded(object sender, RoutedEvent e)
+        {
+            Set_Lenguague();
+        }
+
+        private void dlLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Set_Lenguague();
+        }
+        private void Set_Lenguague()
+        {
+            strLenguague = "More.Idiomas." + ((ComboBoxItem)dllLanguague.SelectedItem).Name.ToString();
+            ResourceManager LocRM = new ResourceManager(strLenguague, typeof(MainWindow).Assembly);
+            lblAge.Text = LocRM.GetString("strAge");
         }
     }
 }
